cf41a8218085dd99b9d0cee5cb7c125eb02573c5
Justin Delgado <delgadojustin100@yahoo.com>
2023-09-24 23:28:03 -0400

    making variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 let titleEl = document.querySelector("#tile");
 let cityForm = document.querySelector("#city-form");
 let cityInput = document.querySelector("#city");
-pastLocations = document.querySelector("#past-locate");
-currentWeather = document.querySelector("#current-weather");
+let pastLocations = document.querySelector("#past-locate");
+let currentWeather = document.querySelector("#current-weather");
