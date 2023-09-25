9f0a5ae888e9fac16916284201e56bd5d5528efc
Justin Delgado <delgadojustin100@yahoo.com>
2023-09-25 14:00:39 -0400

    making the values of the city

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,7 @@ let cityForm = document.querySelector("#city-form");
 let cityInput = document.querySelector("#city");
 let pastLocations = document.querySelector("#past-locate");
 let currentWeather = document.querySelector("#current-weather");
+
+let formSubmit = function (event) {
+    event.preventDefault();
+    let cityName = cityInput.value.trim();
