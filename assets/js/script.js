9beae4667c8aa76353bc2ec6b4d5ea9ea11a1d61
Justin Delgado <delgadojustin100@yahoo.com>
2023-09-26 16:16:38 -0400

    making the fetch request

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,7 @@ let formSubmit = function (event) {
         let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
 
         fetch(queryURL)
-        
+        .then(function)
 
 
 
