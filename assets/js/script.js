0e1a72c482cc4475bfdebe41408d833f075d31a3
Justin Delgado <delgadojustin100@yahoo.com>
2023-10-05 18:22:55 -0400

    making a button for the old locations

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,6 +59,7 @@ let formSubmit = function (event) {
             weatherSearch.textContent = searchCity;
             pastLocations.textContent = searchCity;
             pastLocations.document.createElement("button");
+            pastLocations.appendChild(pastLocations);
             
 
 
