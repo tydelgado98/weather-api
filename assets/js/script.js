8332491d1dadbf98180e1ae33766c7b9853090aa
Justin Delgado <delgadojustin100@yahoo.com>
2023-10-09 22:33:24 -0400

    finishing css

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ let cityInput = document.querySelector("#city");
 let pastLocations = document.querySelector("#past-locate");
 let currentWeather = document.querySelector("#current-weather");
 let weatherSearch = document.querySelector("#weather-term");
+let pTiltel = document.querySelector("#under-tile");
 
 
 let APIKey = "3be2b2b6acc21e3760901d15acf91f72";
@@ -46,7 +47,9 @@ let formSubmit = function (event) {
                 console.log(res);
                 res.json().then(function (data) {
                     console.log(data);
-                    weatherSearch.innerHTML = "";
+                    weatherSearch.innerHTML = `<h2 class="text-center mb-4">5-Day Forecast:</h2>`;
+                    titleEl.textContent = data.city.name;
+                    pTiltel.textContent = "Country: " + data.city.country;
                     displayFiveDays(data, city);
 
                 });
@@ -86,7 +89,7 @@ let formSubmit = function (event) {
                 let dayWeather = weather.list[i].main.temp;
                 console.log(`Temperature for Day ${i + 1}: ${dayWeather}°F`);
                 weatherSearch.innerHTML += `
-                <div class="card text-white bg-primary" style="max-width: 10rem; max-height: 18rem; margin-left: 18px; margin-top:10px; ">
+                <div class="card text-white bg-primary" style="max-width: 10rem; max-height: 18rem; margin-left: 18px; margin-top:10px; padding-bottom: 15px; ">
                 <h5 class="card-title mt-2">${formattedDate}</h5>
                 <img src="http://openweathermap.org/img/w/${weather.list[i].weather[0].icon}.png" alt="weather icon" style="max-width: 8.5rem;">
                 <h6 class="card-text mb-2">Temp : ${weather.list[i].main.temp}°F</h6>
