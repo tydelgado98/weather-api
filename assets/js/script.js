167ebb199a031f19336c0dcc9bd5582e6bd0380c
Justin Delgado <delgadojustin100@yahoo.com>
2023-10-09 22:31:55 -0400

    finishing css

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,7 +87,7 @@ let formSubmit = function (event) {
                 weatherSearch.innerHTML += `
                 <div class="card text-white bg-primary" style="max-width: 10rem;">
                 <h5 class="card-title mt-3">${formattedDate}</h5>
-                <img src="http://openweathermap.org/img/w/${weather.list[i].weather[0].icon}.png" alt="weather icon" style:"60px;">
+                <img src="http://openweathermap.org/img/w/${weather.list[i].weather[0].icon}.png" alt="weather icon" style="max-width: 9rem;">
                 <h6 class="card-text mb-3">Temp : ${weather.list[i].main.temp}°F</h6>
                 <h6 class="card-text mb-3">Wind : ${weather.list[i].wind.speed}MPH</h6>
                 <h6 class="card-text mb-3">Humidity : ${weather.list[i].main.humidity}%</h6>
