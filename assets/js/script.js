0021785c4fcbbfcbac8c4dde4fe1f0b5c625eeb6
Justin Delgado <delgadojustin100@yahoo.com>
2023-10-09 22:33:13 -0400

    finishing css

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,6 +46,7 @@ let formSubmit = function (event) {
                 console.log(res);
                 res.json().then(function (data) {
                     console.log(data);
+                    weatherSearch.innerHTML = "";
                     displayFiveDays(data, city);
 
                 });
@@ -85,7 +86,7 @@ let formSubmit = function (event) {
                 let dayWeather = weather.list[i].main.temp;
                 console.log(`Temperature for Day ${i + 1}: ${dayWeather}°F`);
                 weatherSearch.innerHTML += `
-                <div class="card text-white bg-primary" style="max-width: 10rem; max-height: 18rem; margin-left: 9px; margin-top:10px; ">
+                <div class="card text-white bg-primary" style="max-width: 10rem; max-height: 18rem; margin-left: 18px; margin-top:10px; ">
                 <h5 class="card-title mt-2">${formattedDate}</h5>
                 <img src="http://openweathermap.org/img/w/${weather.list[i].weather[0].icon}.png" alt="weather icon" style="max-width: 8.5rem;">
                 <h6 class="card-text mb-2">Temp : ${weather.list[i].main.temp}°F</h6>
